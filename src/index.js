697d606866170a8ac26a5a8a73a38cdba2215ffc
Serena Inzani <serenainzani@hotmail.com>
2024-03-28 14:35:29 +0000

    config/set up for tests

--- src/index.js
+++ src/index.js
@@ -3,15 +3,12 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App.js";
 import reportWebVitals from "./reportWebVitals.js";
-import cities_data from "./readData.js";
-
-console.log(cities_data);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>
+    // <React.StrictMode>
+    <App />
+    // </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
